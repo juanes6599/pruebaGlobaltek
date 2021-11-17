5d0c9e457e80195815d8081810133e278b3a9795
juanes6599 <47397592+juanes6599@users.noreply.github.com>
2021-11-17 15:52:29 -0500

    Create prueba.js

---
+++ src/prueba.js
@@ -0,0 +1,16 @@
+
+const primerArgumento = [
+    {nombre: 'John', apellido: 'Doe'}, 
+    {nombre: 'Jane', apellido: ''}, 
+    {nombre: 'Jose', apellido: 'Doe'}
+]
+const segundoArgumento =  {apellido: 'Doe'}
+
+function buscar(personas, personaBuscada){
+    key = Object.keys(personaBuscada)[0]
+    value = personaBuscada[key]
+    
+    return personas.filter((persona) => persona[key] == value);
+  }
+
+console.log(buscar(primerArgumento,segundoArgumento))
